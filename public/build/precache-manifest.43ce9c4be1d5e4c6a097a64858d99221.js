58a99577e80d942214147a2988a879e59c43f34f
Boro23 <404slimboysavage@gmail.com>
2020-06-14 16:17:27 +0530

    SpaceX

---
+++ public/build/precache-manifest.43ce9c4be1d5e4c6a097a64858d99221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12af073d5b95c3042e08ed2ae97b071e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0102782e57ad9a0ab1",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c07006cde8c0eed60348",
+    "url": "/static/js/2.f721d3d1.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.f721d3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0102782e57ad9a0ab1",
+    "url": "/static/js/main.834fd1c7.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "f3f39fee699640481e15f0c6f6d157dd",
+    "url": "/static/media/logo.f3f39fee.jpg"
+  }
+]);
